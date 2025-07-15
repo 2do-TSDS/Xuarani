b72ff1bb2fde3f9430707b04e114f363e844aaa5
Nicolás Parísey <nicoparisey@hotmail.com>
2025-07-15 00:57:18 +0000

    destroy scaffold materia

---
+++ app/views/orientacions/_orientacion.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! orientacion, :id, :nombre, :created_at, :updated_at
+json.url orientacion_url(orientacion, format: :json)
---
+++ db/migrate/20250715000408_create_orientacions.rb
@@ -0,0 +1,9 @@
+class CreateOrientacions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :orientacions do |t|
+      t.string :nombre
+
+      t.timestamps
+    end
+  end
+end
