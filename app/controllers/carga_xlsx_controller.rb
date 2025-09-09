14232bce0ef2f5838b5f642dea05ebd4986471af
Andres Fontana <aos.fontana.dev@gmail.com>
2025-09-09 19:58:38 +0000

    Lectura y presentacion de data de xlsx

--- app/controllers/carga_xlsx_controller.rb
+++ app/controllers/carga_xlsx_controller.rb
@@ -3,26 +3,54 @@ require 'daru'
 
 class CargaXlsxController < ApplicationController
   def index
-    @dataframe=nil
+    @division=nil
+    @curso=nil
+    @materias=nil
+    @alumnos=nil
   end
+
 
   def procesar
     archivo_subido = params[:archivo_excel]
 
     if archivo_subido.present?
       ruta_temporal = archivo_subido.tempfile.path
-      
-      begin
-        @dataframe = Daru::DataFrame.from_excel(ruta_temporal)
-        flash.now[:notice] = "Archivo procesado correctamente."
-      rescue => e
-        @dataframe = nil
-        flash.now[:alert] = "Error al leer el archivo: #{e.message}"
+      spreadsheet = Roo::Excelx.new(ruta_temporal)
+
+      alumnos = []
+      curso = ''
+      division = ''
+      materias = []
+      spreadsheet.sheets.each do |sheet_name|
+        sheet = spreadsheet.sheet(sheet_name)
+        case sheet_name
+          when 'Alumnos' then
+            sheet.each_row_streaming() do |row|
+              if row[0].nil?
+                next
+              end
+              alumnos << row[0].cell_value
+            end
+          when 'Curso_Division' then
+            sheet.each_row_streaming() do |row|
+              curso = row[0].cell_value
+              division = row[1].cell_value
+            end
+          when 'Materias' then
+            sheet.each_row_streaming() do |row|
+              if row[0].nil?
+                next
+              end
+              materias << row[0].cell_value
+            end
+        end
       end
-    else
-      flash.now[:alert] = "Por favor, selecciona un archivo."
-    end
-    
+      @alumnos = alumnos
+      @curso = curso
+      @division = division
+      @materias = materias
+      flash.now[:notice] = "Archivo procesado correctamente."
     render :index
+    end
   end
 end
