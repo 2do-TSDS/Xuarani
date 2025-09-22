e34529a4fae6fb1bc1fedc78dfbf6a9b9597da9e
Andres Fontana <aos.fontana.dev@gmail.com>
2025-09-22 21:26:40 +0000

    Arreglo de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 
 ["Administrador", "Preceptor", "Docente", "Alumno"].each do |role_name|
-  Role.find_or_create_by!(name: role_name)
+  Role.find_or_create_by!(nombre: role_name)
 end
 
-puts "Roles creados: #{Role.pluck(:name).join(', ')}"
+puts "Roles creados: #{Role.pluck(:nombre).join(', ')}"
 
 
 admin_email = "admin@admin.com"
@@ -14,7 +14,7 @@ admin = User.find_or_create_by!(email: admin_email) do |u|
   u.password_confirmation = admin_pass
 end
 
-admin_role = Role.find_by!(name: "administrador")
+admin_role = Role.find_by!(nombre: "Administrador")
 admin.roles << admin_role unless admin.roles.include?(admin_role)
 
 puts "Usuario admin creado: #{admin.email} con rol administrador"
